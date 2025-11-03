92e1762aca0eaf01de0b655bb1ac42994e7e5304
Leonardo Pasa <92491691+LeoPasa@users.noreply.github.com>
2025-11-03 21:52:37 +0000

    Estruturação pro exercício

---
+++ src/test/java/com/bcopstein/ex4introducaotesteunitario/FuncionarioTest.java
@@ -0,0 +1,24 @@
+package com.bcopstein.ex4introducaotesteunitario;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EncomendaTest{
+    @Test
+    void salarioLiquidoTeste(){//menor que 5k com INSS
+        
+    }
+    void salarioLiquidoTeste(){//maior que 5k com INSS
+        
+    }
+    void salarioLiquidoTeste(){//menor que 2k500 com INSS
+        
+    }
+    void salarioLiquidoTeste(){//menor que 2k500 com IR
+        
+    }
+        void salarioLiquidoTeste(){//maior que 2k500 com IR
+        
+    }
+}
